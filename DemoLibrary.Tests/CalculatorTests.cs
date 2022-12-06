9fb2d4eb85a1d3679663209fc5f12d3418d3dc97
zormax <okocadere@gmail.com>
2022-12-06 10:08:40 +0300

    project finished

--- DemoLibrary.Tests/CalculatorTests.cs
+++ DemoLibrary.Tests/CalculatorTests.cs
@@ -2,14 +2,42 @@
 
 public class CalculatorTests
 {
-    [Fact]
-    public void Add_SimpleValuesShouldCalculate()
+    [Theory]
+    [InlineData(4, 3, 7)]
+    [InlineData(21, 5.25, 26.25)]
+    [InlineData(double.MaxValue, 5, double.MaxValue)]
+    public void Add_SimpleValuesShouldCalculate(double x, double y, double expected)
     {
         // Arrange
-        double expected = 5;
 
         // Act
-        double actual = Calculator.Add(2, 3);
+        double actual = Calculator.Add(x, y);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(8, 4, 2)]
+    public void Divide_SimpleValuesShouldCalculate(double x, double y, double expected)
+    {
+        // Arrange
+
+        // Act
+        double actual = Calculator.Divide(x, y);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Divide_DivideByZero()
+    {
+        // Arrange
+        double expected = 0;
+
+        // Act
+        double actual = Calculator.Divide(15, 0);
 
         // Assert
         Assert.Equal(expected, actual);
---
+++ DemoLibrary.Tests/ExampleTests.cs
@@ -0,0 +1,16 @@
+﻿namespace DemoLibrary.Tests;
+
+public class ExampleTests
+{
+    [Fact]
+    public void ExampleLoadTextFile_ValidNameShouldWork()
+    {
+        string actual = Examples.ExampleLoadTextFile("This is a valid file name.");
+
+        Assert.True(actual.Length > 0);
+    }
+
+    [Fact]
+    public void ExampleLoadTextFile_InvalidNameShouldFail()
+    { Assert.Throws<ArgumentException>("file", () => Examples.ExampleLoadTextFile(string.Empty)); }
+}
